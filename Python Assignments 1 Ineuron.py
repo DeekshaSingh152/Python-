22882ded7ed5b80bc3d7a4370132ebe26f91a33e
DeekshaSingh152 <121627090+DeekshaSingh152@users.noreply.github.com>
2023-03-23 21:40:28 +0530

    Add files via upload

---
+++ Python Assignments 1 Ineuron.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Programming Assignment_1
+# -------------
+
+# ### 1. Write a Python program to print "Hello Python"?
+
+# ### 2. Write a Python program to do arithmetical operations addition and division.?
+# 
+
+# ### 3. Write a Python program to find the area of a triangle?
+# 
+
+# ### 4. Write a Python program to swap two variables?
+# 
+
+# ### 5. Write a Python program to generate a random number?
+# 
+
+# # 1.PYTHON PROGRAM 
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Python")
+
+
+# In[ ]:
+
+
+
+
+
+# # 2 PYTHON PROGRAM
+
+# In[6]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[7]:
+
+
+a=5
+
+
+# In[8]:
+
+
+b=6
+
+
+# In[9]:
+
+
+a+b ## addition of 2 numbers 
+
+
+# In[14]:
+
+
+a=6 ## division of 2 numbers
+
+
+# In[15]:
+
+
+b=3
+
+
+# In[16]:
+
+
+a/b
+
+
+# ## 3 python program to print area of  the triangle 
+
+# In[20]:
+
+
+def triangle_area(base,height):
+    return 0.5*base*height
+
+
+# In[21]:
+
+
+triangle_area(15,3)
+
+
+# # 4. Write a Python program to swap two variables?
+
+# In[22]:
+
+
+x=6
+y=15
+
+
+# In[23]:
+
+
+x,y=y,x
+
+
+# In[26]:
+
+
+print("x=",x)
+
+
+# In[25]:
+
+
+print("y=",y)
+
+
+# # 5. Write a Python program to generate a random number?
+
+# In[27]:
+
+
+##importing the random module
+import random
+
+
+# In[28]:
+
+
+print(random.randint(0,9))
+
+
+# In[29]:
+
+
+print(random.randint(0,11))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
